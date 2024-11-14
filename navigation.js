625d616edc241b5dba75c2209a2b9567f259e99f
Aliabieva Diana <aliabievadiana25@gmail.com>
2024-11-14 14:14:44 +0100

    navigation

--- navigation.js
+++ navigation.js
@@ -1,8 +1,11 @@
 
 const menuToggle = document.querySelector('#burgerButton');
-const showcase = document.querySelector('#navMenu');
+const showcase = document.querySelector('#mobileMenu');
+// const headContainer = document.querySelector('.head');
 
 menuToggle.addEventListener('click', () => {
   menuToggle.classList.toggle('active');
   showcase.classList.toggle('active');
+  // headContainer.classList.toggle('menu-open');
 })
+
